0de20f072d721bb992091aa90be8d64e95ff525a
r3n4t0r0m3r0 <>
2017-05-09 10:55:05 -0300

    Clase Producto

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php 
+include ('./lib/Producto.php');
+
+$oproducto1 = new Producto();
+$oproducto1->nombre="Nuevo Producto";
+$oproducto1->codigo="001";
+
+$oproducto2 = new Producto("Nuevo Producto 2",0,"002");
+$oproducto3 = new Producto("Nuevo Producto 3",0,"003");
+
+$aproductos[0]=$oproducto1;
+$aproductos[1]=$oproducto2;
+$aproductos[2]=$oproducto3;
+?>
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -11,7 +25,10 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        foreach ($aproductos as $oPro){
+            echo $oPro->codigo." ".$oPro->nombre;
+            echo "<br>";
+        }
         ?>
     </body>
 </html>
---
+++ lib/Producto.php
@@ -0,0 +1,15 @@
+<?php
+
+class Producto {
+    var $nombre="";
+    var $precio=0;
+    public $codigo="";
+    
+    
+    /*Constructor de la clase*/
+    public function __construct($nombre="", $precio=0, $codigo="") {
+        $this->nombre=$nombre;
+        $this->precio=$precio;
+        $this->codigo=$codigo;
+    }
+}
